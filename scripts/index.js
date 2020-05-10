d03cf84f053584cc2267c54c877a6463565c00a1
Vivesh Yadav <viveshofficial@gmail.com>
2020-05-11 02:24:07 +0545

    Add UI for guides

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,23 @@
 const guideList = document.querySelector('.guides');
 // set up guides
 const setupGuides = (data) =>{
-  let html = ``;
-  data.forEach(doc =>{
-    const guide = doc.data();
-    const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4">${guide.title}</div>
-        <div class="collapsible-body white">${guide.content}</div>
-      </li>
-    `;
-    html += li;
+  if(data.length){
+    let html = ``;
+    data.forEach(doc =>{
+      const guide = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">${guide.title}</div>
+          <div class="collapsible-body white">${guide.content}</div>
+        </li>
+      `;
+      html += li;
+    });
     guideList.innerHTML = html;
-  })
+  }else{
+    guideList.innerHTML = '<h4 class="center-align">Login to view guides</h4>';
+  }
+  
 }
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
